4fe9edab25be7372e3dc8d8a58570e02669f210c
YusufOnatYilmaz <76898984+YusufOnatYilmaz@users.noreply.github.com>
2022-02-25 10:35:23 +0300

    Add files via upload

---
+++ her_site_acacagi.py
@@ -0,0 +1,7 @@
+import os
+os.system("start \"\" https://www.youtube.com/")
+os.system("start \"\" https://www.linkedin.com/")
+os.system("start \"\" https://mail.google.com/mail/u/0/#inbox")
+os.system("start \"\" https://www.quora.com/")
+os.system("start \"\" https://mail.yandex.com.tr/#inbox")
+os.system("start \"\" https://metumail.metu.edu.tr/")
